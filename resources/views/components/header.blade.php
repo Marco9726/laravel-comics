23ffb386f346610ffa2adad5d85512d5d8f52db3
Marco 9726 <marcosarra9@gmail.com>
2023-02-16 18:12:18 +0100

    creazione header menu tramite foreach

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,5 +5,27 @@
 			<small class="ms-5"> ADDITIONAL DC SITES &#x25BE;</small>
 		</div>
 	</div>
-	{{print_r($headerMenu)}}
+	<div id="main-header">
+		<div class="container-w60 font-pt">
+			<div class="row align-items-center">
+				<div class="col" id="logo">
+					<h1>LOGO</h1>
+				</div>
+				<nav class="col">
+					<ul class="d-flex">
+						{{-- foreach per ciclare gli elementi dell'array associativo 'headerMenu' --}}
+						@foreach ($headerMenu as $key => $item)
+						<li class="px-3 py-4"> 
+							{{-- valore dell'href = al path definito nell'array in web.php  --}}
+							<a href="{{$item}}">{{$key}}</a>
+						 </li>
+						@endforeach
+					</ul>
+				</nav>
+				<div class="col">
+					SEARCHBAR
+				</div>
+			</div>
+		</div>
+	</div>
 </header>
