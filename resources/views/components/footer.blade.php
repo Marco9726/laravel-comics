a711383223d92b6469585cc023ad9ec00ef674ba
Marco 9726 <marcosarra9@gmail.com>
2023-02-20 19:48:02 +0100

    footer completato

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,6 @@
 <footer>
-	<div id="main-footer" class="py-4">
+	{{-- MAIN-FOOTER  --}}
+	<section id="main-footer" class="py-4">
 		<div class="container-w60 h-100">
 			<div class="row justify-content-between h-100">
 				<div class="col-6 h-100">
@@ -29,5 +30,30 @@
 				</div>
 			</div>
 		</div>
-	</div>
+	</section>
+	{{-- LAST SECTION  --}}
+	<section id="last-section" class="py-4">
+		<div class="container-w60">
+			<div class="row justify-content-between">
+				<div id="button" class="col-2">
+					<button class="px-3 py-2">SING UP NOW!</button>
+				</div>
+				<div id="social" class="col-4 d-flex align-items-center justify-content-end p-0">
+					<h5 class="m-0">FOLLOW US</h5>
+					<ul class="d-flex m-0 gap-3">
+						{{-- foreach per ciclare le icone social  --}}
+						@foreach ($socialArray as $key => $social)
+						<li>
+							<a href="{{ $key }}.com">
+								<img src="{{Vite::asset( $social) }}" alt="{{ $key}}-logo">
+							
+							</a>
+						</li>
+						@endforeach
+
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
 </footer>
